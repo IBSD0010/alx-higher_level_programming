79cbae3b8de5c12dc8326f17813a5fe489314bda
IBSD0010 <ibsd0010@gmail.com>
2024-01-22 16:08:29 -0800

    5-no_c.py  edited

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
 new_string = ""
-for elements in my_string:
-if elements != "c" and elements != "C":
-new_string += elements
+for elements in range(len(my_string)):
+if (my_string[elements] != 'c' and my_string[elements] != 'C':
+new_string += my_string[elements]
 return new_string
